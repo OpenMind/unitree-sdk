7383928f7552aa6d4b5f8c5d4eef4539953998b5
openminddev <147775420+openminddev@users.noreply.github.com>
2025-08-26 16:12:57 -0700

    Update the enable button

--- go2_sdk/launch/slam_launch.py
+++ go2_sdk/launch/slam_launch.py
@@ -155,7 +155,7 @@ def generate_launch_description():
                 'axis_linear.x': 1,
                 'axis_linear.y': 0,
                 'axis_angular.z': 3,
-                'enable_button': 7,
+                'enable_button': 11,
                 'scale_linear.x': 0.5,
                 'scale_angular.z': 1.0
             }]

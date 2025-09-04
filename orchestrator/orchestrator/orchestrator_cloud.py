fda9967c12bdbffa0f0960f3c5b282bee1c723d2
openmindev <boyuan.eth@gmail.com>
2025-09-04 13:47:54 -0700

    Add ENV

--- orchestrator/orchestrator/orchestrator_cloud.py
+++ orchestrator/orchestrator/orchestrator_cloud.py
@@ -10,6 +10,13 @@ import json
 
 from .ws_client import WebSocketClient
 
+env = os.getenv('ENV', 'production')
+if env == 'development':
+    map_api_url = "https://api-dev.openmind.org/api/core/maps/upload"
+    pose_ws_url = "wss://api-dev.openmind.org/api/core/teleops/pose"
+    map_ws_url = "wss://api-dev.openmind.org/api/core/teleops/maps"
+    api_ws_url = "wss://api-dev.openmind.org/api/core/teleops/api"
+
 map_api_url = "https://api.openmind.org/api/core/maps/upload"
 pose_ws_url = "wss://api.openmind.org/api/core/teleops/pose"
 map_ws_url = "wss://api.openmind.org/api/core/teleops/maps"

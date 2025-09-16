4b13d9373e5bd298d2a0045624d73af7d19443ca
openminddev <boyuan.eth@gmail.com>
2025-09-16 16:50:48 -0700

    Fix activate button

--- go2_sdk/launch/slam_launch.py
+++ go2_sdk/launch/slam_launch.py
@@ -162,10 +162,10 @@ def generate_launch_description():
                 'axis_linear.x': 1,
                 'axis_linear.y': 0,
                 'axis_angular.z': 3,
-                'enable_button': 10,
+                'enable_button': 7,
                 'scale_linear.x': 0.5,
                 'scale_angular.z': 1.0,
-                'enable_turbo_button': 9,
+                'enable_turbo_button': 6,
                 'scale_turbo_linear.x': 1.5,
                 'scale_turbo_angular.z': 2.0,
             }]

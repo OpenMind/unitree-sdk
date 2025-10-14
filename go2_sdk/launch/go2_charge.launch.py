845570e6290bb9892da86f34d0a57da864f0c6b1
jerinpeter <jerinpeter@gmail.com>
2025-10-13 18:35:54 -0700

    fixed to work with docker

---
+++ go2_sdk/launch/go2_charge.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    go2_charging_node = Node(
+        package='my_camera_pkg',
+        executable='go2_dock',
+        name='dock_charge_node',
+        output='screen',
+    )
+    return LaunchDescription([
+        go2_charging_node,
+    ])

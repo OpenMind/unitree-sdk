22057c8233bd5828c0d5d1aa5ab065e08a8449cb
YuchengZhou821 <yucheng@openmind.org>
2025-11-13 17:03:15 -0800

    lint

--- orchestrator/orchestrator/core/orchestrator_cloud.py
+++ orchestrator/orchestrator/core/orchestrator_cloud.py
@@ -3,7 +3,13 @@ from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import OccupancyGrid
 from rclpy.node import Node
 
-from om_api.msg import MapStorage, OMAPIRequest, OMAPIResponse, OMASRText, OMAvatarFaceRequest
+from om_api.msg import (
+    MapStorage,
+    OMAPIRequest,
+    OMAPIResponse,
+    OMASRText,
+    OMAvatarFaceRequest,
+)
 
 from ..handlers.ros_handlers import ROSHandlers
 from ..managers.cloud_connection_manager import CloudConnectionManager
@@ -68,7 +74,10 @@ class OrchestratorCloud(Node):
             OMASRText, "/om/asr/text", self.ros_handlers.asr_text_callback, 10
         )
         self.avatar_face_sub = self.create_subscription(
-            OMAvatarFaceRequest, "/om/avatar/face", self.ros_handlers.avatar_face_callback, 10
+            OMAvatarFaceRequest,
+            "/om/avatar/face",
+            self.ros_handlers.avatar_face_callback,
+            10,
         )
 
 
